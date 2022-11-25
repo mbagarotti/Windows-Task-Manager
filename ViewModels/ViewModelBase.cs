f1b015bdc37e915e524eb39c760513405e98ebe2
Marcos Bagarotti <mbagarotti@gmail.com>
2022-11-25 15:47:12 +0800

    Project Created

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_Manager.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Task_Manager.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
