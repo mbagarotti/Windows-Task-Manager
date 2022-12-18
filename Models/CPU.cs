95a904a3cc5f679d16f402c262be9930757278c2
Marcos Bagarotti <mbagarotti@gmail.com>
2022-12-18 18:37:38 +0800

    Other way to calculate CPU freq and Usage. Testing on the data

--- Models/CPU.cs
+++ Models/CPU.cs
@@ -7,6 +7,7 @@ using Task_Manager.Views;
 using System.Diagnostics;
 using System.Threading;
 using Microsoft.VisualBasic;
+using System.Management;
 
 namespace Task_Manager.Models
 {
@@ -14,19 +15,50 @@ namespace Task_Manager.Models
     {
         private static PerformanceCounter cpuusage;
         private static PerformanceCounter cpufrequency;
+        private ManagementObjectSearcher myProcessorObject;
+        private double maxSpeed;
+
 
         public CPU() 
         {
             cpuusage = new PerformanceCounter("Processor Information", "% Processor Utility", "_Total");
-            cpufrequency = new PerformanceCounter("Processor Information", "% of Maximum Frequency", "_Total");
+            cpufrequency = new PerformanceCounter("Processor Information", "% Processor Performance", "_Total");
+            myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor");
+            
+            foreach (ManagementObject obj in myProcessorObject.Get())
+            {
+                System.Diagnostics.Debug.WriteLine("Name  -  " + obj["Name"]);
+                System.Diagnostics.Debug.WriteLine("DeviceID  -  " + obj["DeviceID"]);
+                System.Diagnostics.Debug.WriteLine("Manufacturer  -  " + obj["Manufacturer"]);
+                System.Diagnostics.Debug.WriteLine("CurrentClockSpeed  -  " + obj["CurrentClockSpeed"]);
+                System.Diagnostics.Debug.WriteLine("MaxClockSpeed  -  " + obj["MaxClockSpeed"]);
+                maxSpeed = Convert.ToDouble(obj["MaxClockSpeed"]) / 1000;
+                System.Diagnostics.Debug.WriteLine("Virtualization  -  " + obj["VirtualizationFirmwareEnabled"]);
+                System.Diagnostics.Debug.WriteLine("Number of Threads  -  " + obj["ThreadCount"]);
+                System.Diagnostics.Debug.WriteLine("Caption  -  " + obj["Caption"]);
+                System.Diagnostics.Debug.WriteLine("NumberOfCores  -  " + obj["NumberOfCores"]);
+                System.Diagnostics.Debug.WriteLine("NumberOfEnabledCore  -  " + obj["NumberOfEnabledCore"]);
+                System.Diagnostics.Debug.WriteLine("NumberOfLogicalProcessors  -  " + obj["NumberOfLogicalProcessors"]);
+                System.Diagnostics.Debug.WriteLine("Architecture  -  " + obj["Architecture"]);
+                System.Diagnostics.Debug.WriteLine("Family  -  " + obj["Family"]);
+                System.Diagnostics.Debug.WriteLine("ProcessorType  -  " + obj["ProcessorType"]);
+                System.Diagnostics.Debug.WriteLine("Characteristics  -  " + obj["Characteristics"]);
+                System.Diagnostics.Debug.WriteLine("AddressWidth  -  " + obj["AddressWidth"]);
+                break;
+            }
+
             while (true) {
                 //System.Diagnostics.Debug.WriteLine("First: " + getCurrentCpuUsage() + " %");
                 
                 
-                System.Diagnostics.Debug.WriteLine("Usage: " + getCurrentCpuUsage(250) + " %" + "Frequency: " + (getCurrentCpuFrequency(250)*4.70/100) + "GHz");
+                System.Diagnostics.Debug.WriteLine(getCurrentCpuUsage(500) + "%" +" "+ string.Format("{0:F2}", getCurrentCpuFrequency(500)) + " GHz");
 
                 
             }
+        }
+        public double CPU_MaxFrequency 
+        { 
+            get ;
         }
         public int getCurrentCpuUsage(int miliseconds)
         {
@@ -34,11 +66,11 @@ namespace Task_Manager.Models
             Thread.Sleep(miliseconds);
             return (int)cpuusage.NextValue();
         }
-        public int getCurrentCpuFrequency(int miliseconds)
+        public double getCurrentCpuFrequency(int miliseconds)
         {
             cpufrequency.NextValue();
             Thread.Sleep(miliseconds);
-            return (int)cpufrequency.NextValue();
+            return this.maxSpeed * cpufrequency.NextValue() / 100;
         }
     }
 }
